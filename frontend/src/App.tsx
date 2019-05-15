a79c24db977dd781ae41e33d56794d50d843fbb8
Sasha Hrachova <aleks.gracheva@gmail.com>
2019-05-15 18:53:03 +0100

    Step 2. Building the UI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { Container, Row, Col, CardColumns } from 'react-bootstrap';
+import PetCard from './PetCar';
+
+var mockedPets = [
+  { id: "1", name: "Berty", description: "Has a good nose for truffles" },
+  { id: "2", name: "Argo", description: "A superhero (of the dog world)" },
+  { id: "3", name: "Fred", description: "Has opinions about sausages" },
+];
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <CardColumns>
+          {
+            mockedPets.map((pet) => <PetCard key={pet.id} pet={pet} />)
+          }
+          </CardColumns>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
